eb5c529a1a76e880f015aed0e5a9dd3745328f7a
Edwin Derks <edwin.derks@ordina.nl>
2019-11-08 13:28:47 +0100

    Upgrade project: - Compile with Java 13 - Implement JPMS

---
+++ know-ya-numba-game-logic/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module games.knowyanumba.engine {
+    exports games.knowyanumba.engine;
+    exports games.knowyanumba.exception;
+    exports games.knowyanumba.manager.game;
+    exports games.knowyanumba.manager.score;
+}
---
+++ know-ya-numba-game-swing-client/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module games.knowyanumba.swingclient {
+    requires java.desktop;
+
+    requires games.knowyanumba.engine;
+
+    requires commons.lang3;
+}
